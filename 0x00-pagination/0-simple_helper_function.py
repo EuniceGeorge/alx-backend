6a643e492e9e673f0f0e80484866dffe411edc43
EuniceGeorge <georgeunice408.com>
2024-10-31 12:40:00 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+""" function"""
+
 def index_range(page, page_size):
+    """ index_range"""
     start_page = (page-1) * page_size
     end_page = start_page + page_size
     return (start_page, end_page)
